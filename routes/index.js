89d3c9ebdcc3ce5829670a37fd9b450e0cc6ab82
Tristan Peng <pengt@catlin.edu>
2020-11-03 23:40:48 -0800

    Manage dependencies: remove pixi.js and shortid, add nanoid

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const shortid = require('shortid');
+const nanoid = require('nanoid');
 
 /* GET home page. */
 router.get('/', (req, res) => {
   // res.render('index');
-  res.redirect(`/room/${ shortid.generate() }`);
+  res.redirect(`/room/${ nanoid(10) }`);
 });
 
 module.exports = router;
