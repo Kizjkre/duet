1421ea81df60c9d692a6c7558e115dd7b23015b2
Tristan Peng <pengt@catlin.edu>
2020-11-04 14:33:24 -0800

    Work on room styling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,39 +5,34 @@ const peer = new Peer(undefined, {
 });
 const peers = {};
 
-let i = 0;
-
-const addUser = (video, stream) => {
+const addUser = (video, stream, i) => {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => video.play());
   document.getElementsByClassName('video-container')[i].append(video);
   video.classList.add('has-ratio');
-  i++;
 };
 
 const connect = (user, stream) => {
   const call = peer.call(user, stream);
   const video = document.createElement('video');
-  call.on('stream', incoming => addUser(video, incoming));
+  call.on('stream', incoming => addUser(video, incoming, 1));
   call.on('close', () => video.remove());
   peers[user] = call;
 };
 
-peer.on('open', id => {
-  socket.emit('join', new URL(document.location).pathname.replace('/room/', ''), id);
-});
+peer.on('open', id => socket.emit('join', new URL(document.location).pathname.replace('/room/', ''), id));
 
 socket.on('disconnected', user => peers[user].close());
 
 const video = document.createElement('video');
 video.muted = true;
 
-navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-  addUser(video, stream);
+navigator.mediaDevices.getUserMedia({ video: { width: 1280, height: 720 }, audio: true }).then(stream => {
+  addUser(video, stream, 0);
   peer.on('call', call => {
     const incomingVideo = document.createElement('video');
     call.answer(stream);
-    call.on('stream', incoming => addUser(incomingVideo, incoming));
+    call.on('stream', incoming => addUser(incomingVideo, incoming, 1));
   });
   socket.on('connected', user => connect(user, stream));
 });
