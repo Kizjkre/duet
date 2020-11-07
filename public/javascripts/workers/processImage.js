c35fc00dfb2340c45fcf7333ad062aaa6d1470c3
Tristan Peng <pengt@catlin.edu>
2020-11-06 23:39:03 -0800

    Add luminance and hue calculations, add basic melody picking

--- public/javascripts/workers/processImage.js
+++ public/javascripts/workers/processImage.js
@@ -6,29 +6,70 @@ const sigmoid = (input, { min, max, center, coefficient } = { min: 0, max: 1, ce
 addEventListener('message', async e => {
   const { pixels, activkey } = e.data;
   let avg = 0;
+  let luminance = 0;
+  let hue = 0;
+  let colors = { r: 0, g: 0, b: 0 };
   for (let i = 0; i < pixels.length; i += 4) {
     const [r, g, b] = [pixels[i], pixels[i + 1], pixels[i + 2]];
     avg += (r + g + b) / 3;
+    luminance += 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    colors.r += r / 255;
+    colors.g += g / 255;
+    colors.b += b / 255;
   }
   avg /= pixels.length / 4;
+  luminance /= pixels.length / 4;
+  colors.r /= pixels.length / 4;
+  colors.g /= pixels.length / 4;
+  colors.b /= pixels.length / 4;
+  const max = Math.max(colors.r, colors.g, colors.b);
+  const min = Math.min(colors.r, colors.g, colors.b);
+  switch (max) {
+    case colors.r:
+      hue = 60 * (colors.g - colors.b) / (max - min);
+      break;
+    case colors.g:
+      hue = 60 * (2 + (colors.b - colors.r)) / (max - min);
+      break;
+    case colors.b:
+      hue = 60 * (4 + (colors.r - colors.g)) / (max - min);
+      break;
+  }
+  hue = hue < 0 ? hue + 360 : hue;
 
   const bpm = Math.round(sigmoid(avg, { min: 60, max: 100, center: 0.75 * 255, coefficient: 0.05 }));
-  const rng = mulberry32(xmur3(avg)());
   const chords = await (await fetch('https://api.hooktheory.com/v1/trends/nodes', {
     headers: {
       Authorization : `Bearer ${ activkey }`
     }
   })).json();
-  const choices = [rng(), rng(), rng()];
   let total = 0;
   let chord;
   for (const c of chords) {
     total += c.probability * 1000;
-    if (total > choices[0] * 1000) {
+    if (total > sigmoid(luminance, { min: 0, max: 1, center: 0.75 * 255, coefficient: 0.05 }) * 1000) {
       chord = c;
       break;
     }
   }
-  console.log(chord);
-  const duration = Math.round(sigmoid(choices[1], { min: 0.5, max: 2, center: 0.5, coefficient: 10 }) * 2) / 2;
+  const duration = Math.round(sigmoid(hue, { min: 0.5, max: 2, center: 0.5, coefficient: 10 }) * 2) / 2;
+  const rng = mulberry32(xmur3(avg)());
+  const lookup = await (await fetch('../../assets/lookup.json')).json();
+  const level = Math.round(rng() * lookup[chord.chord_ID].length);
+  const notes = lookup[chord.chord_ID][level];
+  const note = lookup[chord.chord_ID][level][Math.round(rng() * notes.length)];
+  console.dir(chords);
+  console.log(bpm, chord, duration, note);
+  let length;
+  switch (level) {
+    case 0:
+      length = Math.round(sigmoid(rng(), { min: 0.25, max: 1, center: 0.5, coefficient: 10 }) * 8) / 8;
+      break;
+    case 1:
+      length = Math.round(sigmoid(rng(), { min: 0.125, max: 0.5, center: 0.5, coefficient: 10 }) * 8) / 8;
+      break;
+    case 2:
+      length = Math.round(sigmoid(rng(), { min: 0.125, max: 0.25, center: 0.5, coefficient: 10 }) * 8) / 8;
+      break;
+  }
 });
